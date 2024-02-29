eaf99ba2bd818053b47c88ce84861078233d1b16
won <won00yeom@naver.com>
2024-02-29 18:09:36 +0900

    진행중

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,13 +1,8 @@
 const mouse = document.querySelector('.mouse')
 
 // 마우스 올렸을 때 이미지 나타나기
-const thumbnail = document.querySelectorAll('.thumbnail')
-console.log(thumbnail)
-thumbnail.forEach((t, i) =>{
-    t.addEventListener('mouseover',()=>{
-        console.log('.')
-    })
-})
+
+
 
 // 마우스 커서 모양 바꾸기
 window.addEventListener('mousemove',(e)=>{
