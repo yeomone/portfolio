3ffb9ee92a8241be28ea22a4cc01d091bc8af192
won <won00yeom@naver.com>
2024-02-29 17:28:23 +0900

    진행중

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,23 +1,16 @@
 const mouse = document.querySelector('.mouse')
-// const skills_left = document.querySelectorAll('.skills_left li a')
-// const skills_right = document.querySelectorAll('.skills_right li')
-// const skill_works = document.querySelectorAll('.skill_works p')
 
+// 마우스 올렸을 때 이미지 나타나기
+const thumbnail = document.querySelectorAll('.thumbnail')
+console.log(thumbnail)
+thumbnail.forEach((t, i) =>{
+    t.addEventListener('mouseover',()=>{
+        console.log('.')
+    })
+})
+
+// 마우스 커서 모양 바꾸기
 window.addEventListener('mousemove',(e)=>{
     mouse.style.left = `${e.clientX}px`
     mouse.style.top = `${e.clientY}px`
 })
-
-/* const hide = () => {for(let r of skills_right){r.style.display = 'none'}}
-hide()
-
-skills_left.forEach((t, i)=>{
-    const border_hide = () => {for(let h of skills_left)h.style.borderBottom = '0'}
-    t.addEventListener('click',(e)=>{
-        e.preventDefault()
-        border_hide()
-        t.style.borderBottom = '1px solid #000'
-        hide()
-        skills_right[i].style.display = 'block'
-    })
-}) */
