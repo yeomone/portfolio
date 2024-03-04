f991638ea18bba864ae48a4ad2ab5001114de616
won <won00yeom@naver.com>
2024-03-04 17:22:02 +0900

    마우스 이미지 추가

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,10 +1,35 @@
 const mouse = document.querySelector('.mouse')
+const introContents = document.querySelector('.introContents')
+const contact = document.querySelector('.contact')
+const image = document.querySelector('.mouse img'); // 이미지 요소 선택
 
 // 마우스 커서 모양 바꾸기
 window.addEventListener('mousemove',(e)=>{
     mouse.style.left = `${e.clientX}px`
     mouse.style.top = `${e.clientY}px`
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // 특정 영역에 들어갔을 때 이미지 경로 변경
+    introContents.addEventListener("mouseenter", function() {
+        image.src = "./images/mouse_white.png";
+    });
+    
+    // 특정 영역에서 나갔을 때 이미지 경로 원래대로 복원
+    introContents.addEventListener("mouseleave", function() {
+        image.src = "./images/mouse.png";
+    });
+    // 특정 영역에 들어갔을 때 이미지 경로 변경
+    contact.addEventListener("mouseenter", function() {
+        image.src = "./images/mouse_white.png";
+    });
+    
+    // 특정 영역에서 나갔을 때 이미지 경로 원래대로 복원
+    contact.addEventListener("mouseleave", function() {
+        image.src = "./images/mouse.png";
+    });
+});
 
 //  ------- 링크 설정되지 않은 a 태그 클릭시 상단으로 올라가는 것 막기
 document.querySelectorAll('a').forEach(link => {
